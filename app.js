43d545416ca6b965923b1826d020028d7223426c
Mengqi777 <memgqi@live.cn>
2018-02-23 22:42:11 +0800

    add sleepstory

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-    
+    var that=this;
     var innerac = this.globalData.innerac;
     innerac = wx.createInnerAudioContext();
     innerac.loop=true;
     innerac.src="/music/ROS.mp3";
     innerac.play();
-    this.globalData.innerac=innerac;
+    that.globalData.innerac=innerac;
     // 登录
     wx.login({
       success: res => {
@@ -28,42 +28,42 @@ App({
                 wx.getUserInfo({
                   success: res => {
                     // 可以将 res 发送给后台解码出 unionId
-                    this.globalData.userInfo = res.userInfo
+                    that.globalData.userInfo = res.userInfo
                     console.log(res)
                     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-                    wx.request({
-                      url: server+'/customer/login',
-                      method: 'POST',
-                      data: {
-                        userInfo: res.rawData,
-                        code: code
-                      }, header: {
-                        'content-type': 'application/x-www-form-urlencoded'
-                      },
-                      success: function (res) {
-                        var customer=res.data.customer;
-                        wx.setStorageSync('customer', customer);
-                        var pets=customer.pets||[];
-                        for(var i=0;i<pets.length;i++){
-                          if(pets[i].type==0){
-                            wx.setStorageSync('px', pets[i])
-                          }
-                          if(pets[i].type==1){
-                            wx.setStorageSync('dog', pets[i])
-                          }
-                        }
-                        if (customer.pets == null || customer.pets.length==0){
-                          wx.reLaunch({
-                            url: '/pages/index/index',
-                          })
-                        }else{
-                          wx.reLaunch({
-                            url: '/pages/home/home',
-                          })
-                        }
-                      }
+                    // wx.request({
+                    //   url: server+'/customer/login',
+                    //   method: 'POST',
+                    //   data: {
+                    //     userInfo: res.rawData,
+                    //     code: code
+                    //   }, header: {
+                    //     'content-type': 'application/x-www-form-urlencoded'
+                    //   },
+                    //   success: function (res) {
+                    //     var customer=res.data.customer;
+                    //     wx.setStorageSync('customer', customer);
+                    //     var pets=customer.pets||[];
+                    //     for(var i=0;i<pets.length;i++){
+                    //       if(pets[i].type==0){
+                    //         wx.setStorageSync('px', pets[i])
+                    //       }
+                    //       if(pets[i].type==1){
+                    //         wx.setStorageSync('dog', pets[i])
+                    //       }
+                    //     }
+                    //     if (customer.pets == null || customer.pets.length==0){
+                    //       wx.reLaunch({
+                    //         url: '/pages/index/index',
+                    //       })
+                    //     }else{
+                    //       wx.reLaunch({
+                    //         url: '/pages/home/home',
+                    //       })
+                    //     }
+                    //   }
 
-                    })
+                    // })
                     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
                     // 所以此处加入 callback 以防止这种情况
                     if (this.userInfoReadyCallback) {
---
+++ pages/storydetail/storydetail.js
@@ -0,0 +1,144 @@
+// pages/storydetail/storydetail.js
+var server = "https://api.mengqipoet.cn"
+// var server = "http://localhost:8080"
+var util = require("../../utils/util");
+const app=getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    hadshowwt: false,
+    story: {},
+    userInfo:{},
+    title:"",
+    content:""
+  },
+  playgame:function(){
+    wx.navigateTo({
+      url: '/pages/home/home',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that=this;
+    wx.getUserInfo({
+      success: res => {
+        that.setData({
+          userInfo:res.userInfo
+        })
+      }});
+    var dt = null;
+    if (options.datetime === undefined) {
+      var datt = new Date();
+      dt = util.formatTime(datt).split(" ")[0];
+      console.log(dt)
+    } else {
+      dt = options.datetime;
+    }
+   
+    wx.request({
+      url: server + '/sleepstory/getbydatetime',
+      method: 'GET',
+      data: {
+        dateTime: dt
+      }, header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        that.setData({
+          story:res.data
+        })
+      }
+    });
+  },
+  bindtitle:function(e){
+    this.setData({
+      title:e.detail.value
+    })
+  },
+  bindcontent: function (e) {
+    this.setData({
+      content: e.detail.value
+    })
+  },
+  savestory: function () {
+    var datt=new Date();
+    var dt=util.formatTime(datt);
+    var mystory={}
+    var that=this;
+    mystory.dateTime=dt;
+    mystory.author=that.data.userInfo.nickName;
+    mystory.content = that.data.content;
+    mystory.title=that.data.title;
+    console.log(mystory)
+    wx.request({
+      url: server + '/sleepstory/add',
+      method: 'POST',
+      data: mystory, 
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data);
+      }
+    });
+  },
+  showwriteone: function () {
+    var that = this;
+    that.setData({
+      hadshowwt: that.data.hadshowwt ? false : true
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
